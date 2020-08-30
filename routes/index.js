15c5782f3a7ca810e2760fb5b89a0a8da60aefb4
김희윤 <dnflwlq3231@naver.com>
2020-08-30 20:45:47 +0900

    test aws s3

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,11 @@ const multer = require('multer');
 const multerS3 = require('multer-s3');
 const AWS = require('aws-sdk');
 const path = require('path');
-const s3 = new AWS.S3();
 AWS.config.loadFromPath('/Users/corgi/Desktop/sos_homepage/config/awsconfig.json');
 // AWS.config.loadFromPath('/root/sos_homepage/config/awsconfig.json');
+const s3 = new AWS.S3();
 
-const upload = multer({
+const uploadOption = multer({
   storage: multerS3({
       s3: s3,
       bucket: 'sos-homepage-s3/test',
@@ -20,6 +20,11 @@ const upload = multer({
   })
 });
 
+const getObjectParam = {
+  Bucket: "sos-homepage-s3",
+  Key: "default"
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -30,14 +35,15 @@ router.post('/postImg', upload.single('img'), (req, res, next) => {
 })
 
 router.post('/list', (req, res, next) => {
-  const response = s3.listObjectsV2({
-    Bucket: 'test'
-  }, (err, data) => {
+  let param = {
+    Bucket: 'sos-homepage-s3'
+  }
+  const response = s3.listObjectsV2(param, (err, data) => {
     if (err) {
-      console.log(err)
+      console.log('err \n', err)
     }
     else {
-      return data
+      console.log('data \n', data)
     }
   })
   res.send(response)
