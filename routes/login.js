8bb3c1ad9e2498f147c283290215696fe0046a69
김희윤 <dnflwlq3231@naver.com>
2020-09-01 00:00:26 +0900

    add front and set login proccess

--- routes/login.js
+++ routes/login.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var router = express.Router();
-const AWS = require('aws-sdk');
-//AWS.config.loadFromPath('/Users/corgi/Desktop/sos_homepage/config/awsconfig.json');
-//AWS.config.loadFromPath('/root/sos_homepage/config/awsconfig.json');
+var AWS = require('aws-sdk');
+var config = require('/Users/corgi/Desktop/sos_homepage/config/awsconfig.json');
+//var config = require('/root/sos_homepage/config/awsconfig.json');
 AWS.config.update({
   region: 'ap-northeast-2',
+  accessKeyId: config.accessKeyId,
+  secretAccessKey: config.secretAccessKey
   //endpoint: 'http://localhost:3000/login'
-  endpoint: 'http://13.125.0.37'
+  //endpoint: 'http://13.125.0.37'
   //endpoint: 'http://172.31.47.65'
 });
-const dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+var dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
